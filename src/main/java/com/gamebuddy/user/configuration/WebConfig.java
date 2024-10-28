91f4f295a6a834efb0c054ae64da9ae6a7c1a72c
Görkem <gorkem_yelken@hotmail.com>
2024-10-28 23:17:06 +0300

    WebConfig added

---
+++ src/main/java/com/gamebuddy/user/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.gamebuddy.user.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Tüm endpoint'ler için CORS'u etkinleştir
+                .allowedOrigins("*") // Tüm kaynaklara izin ver
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // İzin verilen HTTP yöntemleri
+                .allowedHeaders("*") // İzin verilen başlıklar
+                .allowCredentials(true); // Kimlik bilgilerini kullanmaya izin ver
+    }
+}
