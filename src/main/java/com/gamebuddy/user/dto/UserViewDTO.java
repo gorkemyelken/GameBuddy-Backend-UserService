508c966f5febae8f7a804776809d7847ec552c8e
Görkem <gorkem_yelken@hotmail.com>
2024-10-31 14:54:37 +0300

    average rating updated

--- src/main/java/com/gamebuddy/user/dto/UserViewDTO.java
+++ src/main/java/com/gamebuddy/user/dto/UserViewDTO.java
@@ -19,5 +19,5 @@ public class UserViewDTO {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private Set<LanguagePreference> preferredLanguages;
-    private Integer averageRating;
+    private Float averageRating;
 }
